d2c64966db0dad5ebb940a9a062613f8ff8b828c
Alexander <alexander.kraus@informatik.uni-mannheim.de>
2023-03-06 10:28:14 +0100

    fixing drift info collection

--- controllers/drift_info_collection.py
+++ controllers/drift_info_collection.py
@@ -1,107 +1,192 @@
 from dataclasses import dataclass
+import datetime
+import glob, os
+import pm4py
 
 @dataclass
 class DriftInfo:
-    """
-        Object for keeping information of a single drift added to an event log
-    """
-    log_id: str  # unique name of the log to which the drift belongs
-    drift_id: int  # unique per log (1, 2, 3, ...)
-    process_perspective: str  # it is always control-flow for now
-    drift_type: str  # sudden, gradual, incremental, or recurring
-    drift_time: list  # list with at least one timestamp as a string
-    activities_added: list  # list of activity labels as str (might be a list of list)
-    activities_deleted: list  # list of activity labels as str (might be a list of list)
-    activities_moved: list  # list of activity labels as str (might be a list of list)
+    log_id: int
+    drift_id: int # One drift per log so drift_id is always 1
+    process_perspective:str
+    drift_type: str
+    drift_time:dict # With one timestamp or two timestamps according to the drift type so that the drift instantiation class is done in a single line
+    activities_added:dict # {"act added 1": ,...}
+    activities_deleted:dict
+    activities_moved:dict
+
+    def __post_init__(self):
+        if self.process_perspective not in ["control-flow"]:
+            raise ValueError("wrong value inserted for process_perspective")
+
+        if self.drift_type not in ["sudden", "gradual", "incremental", "recurring"]:
+            raise ValueError("wrong drift type")
+
+        if (type(self.log_id) != str):
+            raise TypeError
+
+        if (type(self.drift_id) != int):
+            raise TypeError
+
+        if (type(self.process_perspective) != str):
+            raise TypeError
+
+        if (type(self.drift_type) != str):
+            raise TypeError
+
+        if (type(self.drift_time) != list): #should be a list
+            raise TypeError
+
+        if (type(self.activities_added) != list): #should be a list:
+            raise TypeError
+
+        if (type(self.activities_deleted) != list): #should be a list
+            raise TypeError
+
+        if (type(self.activities_moved) != list): #should be a list
+            raise TypeError
+
+        if self.drift_type == "sudden":
+            self.drift_time.remove('N/A')
+
+        if self.drift_type !="sudden":
+            self.drift_time[1] = datetime.datetime.strptime(self.drift_time[1][0:len(self.drift_time[1])-6].strip(),"%Y-%d-%m %H:%M:%S")
+
+        self.drift_id=0 #Once we generate multiple drifts this should be changed
+
+    def drift_info_to_dict(self):
+        DI = vars(self)
+        d = dict()
+        d["value"] = True ## By default need to add this as a parameter
+        d["children"] = dict({i[0]:i[1] for i in zip(list(DI.keys()), list(DI.values()))})
+        return d
 
 @dataclass
 class NoiseInfo:
     """
         Object for keeping information about added noise to a generated event log
     """
-    log_id: str  # unique name of the log to which the drift belongs
+    log_id: int  # unique name of the log to which the drift belongs
     noise_id: int  # unique per log
     noise_perspective: str  # control-flow
     noise_type: str  # like random_model
     noise_proportion: float  # 0.05
-    noise_start: str  # timestamp like 2020-03-27 05:32:12
-    noise_end: str  # timestamp like 2020-08-21 07:05:11
+    noise_start: datetime.datetime  # timestamp like 2020-03-27 05:32:12
+    noise_end: datetime.datetime # timestamp like 2020-08-21 07:05:11
+
+    def __post_init__(self):
+        if (type(self.log_id)!=str):
+            raise TypeError
+
+        if (type(self.noise_id)!=int):
+            raise TypeError
+
+        if (type(self.noise_perspective) != str):
+            raise TypeError
+
+        if (type(self.noise_type) != str):
+            raise TypeError
+
+        if (type(self.noise_proportion) != float):
+            raise TypeError
+
+        if (type(self.noise_start)!=datetime.datetime):
+            raise TypeError
+
+        if (type(self.noise_end) != datetime.datetime):
+            raise TypeError
+
+        self.noise_id=0 #Once we generate multiple drifts this should be changed
+    def noise_info_to_dict(self):
+        NI = vars(self)
+        d = dict()
+        d["value"] = True
+        d["children"] = dict({i[0]: i[1] for i in zip(list(NI.keys()), list(NI.values()))})
+        return d
+
+
+
 
 @dataclass
 class LogDriftInfo:
     """
         Object for keeping information about added drift and noise instances for a generated event log
     """
-    log_id: str  # unique name of the log to which the drift belongs
-    number_of_drifts: int # initially 0
-    number_of_noises: int # initially 0
+    drifts = list() #list[DriftInfo]
+    noise = list()
+    number_of_drifts: int = 0  # initially 0
+    number_of_noises: int = 0  # initially 0
 
-    drifts: list[DriftInfo]
-    noise: list[NoiseInfo]
+    def add_drift(self, dinf):
+        self.drifts.append(dinf)
 
-    def add_drift(self):
-        # TODO
-        pass
-
-    def add_noise(self):
-        # TODO
-        pass
+    def add_noise(self, ninf):
+        self.noise.append(ninf)
 
     def increase_drift_count(self):
-        # TODO
-        pass
+        self.number_of_drifts+=1
 
     def increase_noise_count(self):
-        # TODO
-        pass
+        self.number_of_noises+=1
 
 
-# IGNORE THINGS BELOW
-# @dataclass
-# class CollectionLogDriftInfo:
-#     """
-#         Object for keeping information about drift information accros all generated event logs
-#     """
-#
-#     def export_to_json(self):
-#         pass
 
-# "perspective: control-flow; " \
-# "type: sudden; " \
-# "drift_moment: 2020-04-01 10:01:57 (0.4); " \
-# "activities_added: [Random activity 1, Random activity 2, Random activity 3]; " \
-# "activities_deleted: []; " \
-# "activities_moved: []"
-#
-# "perspective: control-flow;"
-# " type: gradual;"
-# " specific_information: linear distribution;"
-# " drift_start: 2020-10-08 21:11:36 (0.4);"
-# " drift_end: 2021-01-25 08:13:53 (0.6); "
-# "activities_added: [Random activity 1]; "
-# "activities_deleted: [a]; "
-# "activities_moved: []"
-#
-# "perspective: control-flow; "
-# "type: incremental; "
-# "specific_information: 5 versions of the process model;"
-# " drift_start: 2020-03-23 18:40:23 (0.33);"
-# "drift_end: 2020-04-04 21:57:37 (0.67);"
-# "activities_added: [Random activity 1, Random activity 2];"
-# "activities_deleted: [h, k, e, b];"
-# "activities_moved: [c, f]"
-#
-# "perspective: control-flow; "
-# "type: recurring; "
-# "specific_information: 3 seasonal_changes;"
-# "drift_start: 2020-04-13 09:42:30 (0.2);"
-# "drift_end: 2020-08-03 13:43:19 (0.8);"
-# " activities_added: [Random activity 1];"
-# "activities_deleted: [g];
-# "activities_moved: []"
-#
-# "noise_info:"
-# "noise_proportion: 0.05;"
-# "start_point: 2020-03-27 05:32:12 (0.1);"
-# "end_point: 2020-08-21 07:05:11 (0.9);"
-# "noise_type: random_model"
+def DriftInfo_from_xes(path):
+    #C: / Users / ziedk / OneDrive / Bureau / New
+    #folder / cdlg_tool_dev / data / generated_collections / 1677683533
+    drifts = list()
+    os.chdir(path)
+    for file in glob.glob("*.xes"):
+        if __name__ == "__main__":
+            log = pm4py.read_xes(path+"/"+str(file))
+        DI = DriftInfo(*list(log[list(log.attributes.keys())[0]]["children"].values()))
+        drifts.append(DI)
+
+    return drifts
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
