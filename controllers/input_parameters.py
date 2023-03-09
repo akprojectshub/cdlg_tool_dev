c870ffc6715edd344d2b2b7e49b3c2e6eb6719ce
Alexander <alexander.kraus@informatik.uni-mannheim.de>
2023-03-09 15:39:09 +0100

    Updated input parameters list and corresponding dataclass

--- controllers/input_parameters.py
+++ controllers/input_parameters.py
@@ -7,12 +7,12 @@ from random import randint, uniform, choice
 @dataclass
 class InputParameters:
 
-    Complexity_random_tree: list
+    Process_tree_complexity: list
+    Process_tree_evolution_proportion: list
     Number_event_logs: int
     Number_traces_per_event_log: list
-    Drifts: list
+    Drift_types: list
     Drift_area: list
-    Proportion_random_evolution_sector: list
     Noise: list
     Noisy_trace_prob: list
     Noisy_event_prob: list
@@ -21,14 +21,10 @@ class InputParameters:
     Task_exp_duration_sec: list
     Gradual_drift_type: list
     Incremental_drift_number: list
-    Recurring_drift_seasons: list
-
+    Recurring_drift_number: list
 
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
             if key in {field.name for field in fields(InputParameters)}:
                 setattr(self, key, value)
-
-
-
 
