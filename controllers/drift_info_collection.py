6d5e04df0c6b286536fd2cfc7fb3ac8f2d6a45df
Alexander <alexander.kraus@informatik.uni-mannheim.de>
2023-03-12 20:32:05 +0100

    Updated drift info collection script

--- controllers/drift_info_collection.py
+++ controllers/drift_info_collection.py
@@ -1,51 +1,23 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, fields
 import datetime
 import glob, os
+
+import numpy as np
 import pm4py
 
+
+@dataclass
 class DriftInfo:
-    def __init__(self, dictionary):
-        for k, v in dictionary.items():
-            setattr(self, k, v)
+    log_id: int = np.NAN
+    drift_id: int = np.NAN
+    process_perspective: str = 'none'
+    drift_type: str = 'none'
+    drift_time: list = list
+    activities_added: list = list
+    activities_deleted: list = list
+    activities_moved: list = list
+    process_trees: list = list
 
-    def __post_init__(self):
-        if self.process_perspective not in ["control-flow"]:
-            raise ValueError("wrong value inserted for process_perspective")
-
-        if self.drift_type not in ["sudden", "gradual", "incremental", "recurring"]:
-            raise ValueError("wrong drift type")
-
-        if (type(self.log_id) != str):
-            raise TypeError
-
-        if (type(self.drift_id) != int):
-            raise TypeError
-
-        if (type(self.process_perspective) != str):
-            raise TypeError
-
-        if (type(self.drift_type) != str):
-            raise TypeError
-
-        if (type(self.drift_time) != list): #should be a list
-            raise TypeError
-
-        if (type(self.activities_added) != list): #should be a list:
-            raise TypeError
-
-        if (type(self.activities_deleted) != list): #should be a list
-            raise TypeError
-
-        if (type(self.activities_moved) != list): #should be a list
-            raise TypeError
-
-        if self.drift_type == "sudden" and len(self.drift_time)>1:
-            self.drift_time.remove('N/A')
-
-        if self.drift_type !="sudden" and type(self.drift_time[1]).__name__!='datetime':
-            self.drift_time[1] = datetime.datetime.strptime(self.drift_time[1][0:len(self.drift_time[1])-6].strip(),"%Y-%m-%d %H:%M:%S.%f")
-
-        self.drift_id=0 #Once we generate multiple drifts this should be changed
 
     def drift_info_to_dict(self):
         DI = vars(self)
@@ -55,13 +27,13 @@ class DriftInfo:
         d = dict()
         d["value"] = True  ## By default need to add this as a parameter
 
-        d_final =  {"value":True, "children":{}}
+        d_final = {"value": True, "children": {}}
 
         for i in range(0, len(type_list)):
             if type_list[i] == 'list':
-                d[keys_list[i]] = {keys_list[i] + "_" + str(j+1): values_list[i][j] for j in
+                d[keys_list[i]] = {keys_list[i] + "_" + str(j + 1): values_list[i][j] for j in
                                    range(0, len(values_list[i]))}
-                d_final["children"][keys_list[i]] = {"value":values_list[i],"children":d[keys_list[i]]}
+                d_final["children"][keys_list[i]] = {"value": values_list[i], "children": d[keys_list[i]]}
 
             elif type_list[i] != 'list':
                 d[keys_list[i]] = values_list[i]
@@ -76,14 +48,13 @@ class DriftInfo:
         xes = log.attributes["drift:info"]["children"]
 
         for key, value in xes.items():
-            if value == 0 and key!="drift_id":
+            if value == 0 and key != "drift_id":
                 d[key] = []
-            elif (type(value).__name__!='dict'):
+            elif (type(value).__name__ != 'dict'):
                 d[key] = value
             else:
                 d[key] = list(value["children"].values())
         return d
-
 
 
 @dataclass
@@ -91,32 +62,11 @@ class NoiseInfo:
     """
         Object for keeping information about added noise to a generated event log
     """
-    def __init__(self, dictionary):
-        for k, v in dictionary.items():
-            setattr(self, k, v)
-    def __post_init__(self):
-        if (type(self.log_id)!=str):
-            raise TypeError
+    log_id: int = np.NAN
+    noisy_trace_prob: float = np.NAN
+    noisy_event_prob: float = np.NAN
 
-        if (type(self.noise_id)!=int):
-            raise TypeError
 
-        if (type(self.noise_perspective) != str):
-            raise TypeError
-
-        if (type(self.noise_type) != str):
-            raise TypeError
-
-        if (type(self.noise_proportion) != float):
-            raise TypeError
-
-        if (type(self.noise_start)!=datetime.datetime):
-            raise TypeError
-
-        if (type(self.noise_end) != datetime.datetime):
-            raise TypeError
-
-        self.noise_id=0 #Once we generate multiple drifts this should be changed
     def noise_info_to_dict(self):
         NI = vars(self)
         keys_list = list(NI.keys())
@@ -153,30 +103,32 @@ class NoiseInfo:
         return d
 
 
-
 @dataclass
 class LogDriftInfo:
     """
     Object for keeping information about added drift and noise instances for a generated event log
     """
-    drifts = list() #list[DriftInfo]
+    drifts = list()
     noise = list()
-    number_of_drifts: int = 0  # initially 0
-    number_of_noises: int = 0  # initially 0
+    number_of_drifts: int = 0
+    number_of_noises: int = 0
 
-    def add_drift(self, dinf):
-        self.drifts.append(dinf)
+    def add_drift(self, instance: DriftInfo):
+        self.drifts.append(instance)
+        self.increase_drift_count()
 
     def add_noise(self, ninf):
         self.noise.append(ninf)
+        self.increase_drift_count()
 
     def increase_drift_count(self):
-        self.number_of_drifts+=1
+        self.number_of_drifts += 1
 
     def increase_noise_count(self):
-        self.number_of_noises+=1
+        self.number_of_noises += 1
 
-    def fill_drift_log(self): #This method is used to store the dictionary with log attribute levels in the log (xes file)
+    def fill_drift_log(
+            self):  # This method is used to store the dictionary with log attribute levels in the log (xes file)
         param_drift = vars(DriftInfo)
 
     @staticmethod
@@ -189,7 +141,7 @@ class LogDriftInfo:
 
         read_class = list()
 
-        for p in [v+"/"+k for k,v in loaded_event_logs.items()]:
+        for p in [v + "/" + k for k, v in loaded_event_logs.items()]:
             log = pm4py.read_xes(p)
             DI = DriftInfo(DriftInfo.extract_info_xes(log))
             read_class.append(DI)
@@ -211,7 +163,8 @@ class LogDriftInfo:
             read_class.append(NI)
         return read_class
 
-def extract_change_moments(created_log):
+
+def extract_change_moments_to_dict(created_log):
     change_moments = {}
     current_model_version = created_log[0].attributes['model:version']
     change_id = 0
@@ -223,3 +176,17 @@ def extract_change_moments(created_log):
             change_moments['change_' + str(change_id)] = change_moment  # .strftime("%Y-%m-%d, %H:%M:%S")
             current_model_version = version
     return {"value": "timestamps", "children": change_moments}
+
+
+def extract_change_moments_to_list(created_log):
+    change_moments = []
+    current_model_version = created_log[0].attributes['model:version']
+    change_id = 0
+    for trace in created_log:
+        change_moment = trace[0]['time:timestamp']
+        version = trace.attributes['model:version']
+        if version != current_model_version:
+            change_id += 1
+            change_moments.append(change_moment)  # .strftime("%Y-%m-%d, %H:%M:%S"))
+            current_model_version = version
+    return change_moments
