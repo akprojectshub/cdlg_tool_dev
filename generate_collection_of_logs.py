9b17cac614d64a63cc19c5daa140aa93393a4037
Alexander <alexander.kraus@informatik.uni-mannheim.de>
2023-03-27 11:10:39 +0200

    Updated main script: multi-sudden drifts per log are added (WIP)

--- generate_collection_of_logs.py
+++ generate_collection_of_logs.py
@@ -21,6 +21,7 @@ from controllers.drift_info_collection import LogDriftInfo
 from controllers.process_tree_controller import generate_tree_from_file, generate_specific_trees
 from pm4py.objects.log.exporter.xes import exporter as xes_exporter
 from controllers.input_parameters import InputParameters
+from concept_drifts.change_types import add_sudden_change
 import time
 
 
@@ -42,35 +43,42 @@ def generate_logs(file_path_to_own_models=None):
     collection = LogDriftInfo()
     for log_id in range(1, number_of_logs + 1):
         # SELECT PARAMETERS FOR THE CURRENT LOG
-        log_name = "log_"  + str(log_id) + '_' + str(int(time.time())) + ".xes"
-        num_traces = select_random(par.Number_traces_per_event_log, option='uniform_int')
+        log_name = "log_" + str(log_id) + '_' + str(int(time.time())) + ".xes"
         tree_one, complexity = generate_initial_tree(par.Process_tree_complexity, file_path_to_own_models)
-        drift = select_random(par.Drift_types, option='random')
-        noise = select_random(par.Noise, option='random')
+        tree_list = [tree_one]
+        num_traces = select_random(par.Number_traces_per_event_log, option='uniform_int')
+        event_log = no_drift(tree=tree_one, nu_traces=num_traces)
 
-        drift_area_one, drift_area_two = drift_area_selection(par.Drift_area)
-        ran_evolve = select_random(par.Process_tree_evolution_proportion, option='uniform')
 
-        tree_two, deleted_acs, added_acs, moved_acs = evolve_tree_randomly(copy.deepcopy(tree_one), ran_evolve)
-        tree_list = [tree_one, tree_two]
+        drift_n = select_random(par.Number_drifts_per_log, option='uniform_int')
+        for drift_id in range(1, drift_n+1):
+            tree_previous = copy.deepcopy(tree_list[-1])
+            drift = select_random(par.Drift_types, option='random')
+            drift_area_one, drift_area_two = drift_area_selection(par.Drift_area)
+            ran_evolve = select_random(par.Process_tree_evolution_proportion, option='uniform')
+            tree_new, deleted_acs, added_acs, moved_acs = evolve_tree_randomly(tree_previous, ran_evolve)
+            num_traces = select_random(par.Number_traces_per_event_log, option='uniform_int')
+            tree_list.append(tree_new)
 
-        # GENERATE LOG WITH A CERTAIN DRIFT TYPE
-        if drift == DriftTypes.sudden.value:
-            event_log = sudden_drift(tree_one, tree_two, num_traces, drift_area_one)
-        elif drift == DriftTypes.gradual.value:
-            gr_type = select_random(par.Gradual_drift_type, option='random')
-            event_log = gradual_drift(tree_one, tree_two, num_traces, drift_area_one, drift_area_two, gr_type)
-        elif drift == DriftTypes.recurring.value:
-            number_of_seasonal_changes = select_random(par.Recurring_drift_number, option='random')
-            event_log = recurring_drift_new(tree_one, tree_two, num_traces, number_of_seasonal_changes)
-        elif drift == DriftTypes.incremental.value:
-            # TODO: rewrite the incremental drift generation function (focus on simplification)
-            num_models = select_random(par.Incremental_drift_number, option='random')
-            result = incremental_drift_gs(tree_one, drift_area_one, drift_area_two, num_traces, num_models, ran_evolve)
-            event_log, deleted_acs, added_acs, moved_acs, tree_list = result
-        else:
-            event_log = no_drift(tree=tree_one, nu_traces=num_traces)
-            drift = None
+            # GENERATE LOG WITH A CERTAIN DRIFT TYPE
+            if drift == DriftTypes.sudden.value:
+                #event_log = sudden_drift(tree_previous, tree_new, num_traces, drift_area_one)
+                event_log = add_sudden_change(event_log, tree_new, num_traces)
+
+            elif drift == DriftTypes.gradual.value:
+                gr_type = select_random(par.Gradual_drift_type, option='random')
+                event_log = gradual_drift(tree_previous, tree_new, num_traces, drift_area_one, drift_area_two, gr_type)
+            elif drift == DriftTypes.recurring.value:
+                number_of_seasonal_changes = select_random(par.Recurring_drift_number, option='random')
+                event_log = recurring_drift_new(tree_previous, tree_new, num_traces, number_of_seasonal_changes)
+            elif drift == DriftTypes.incremental.value:
+                # TODO: rewrite the incremental drift generation function (focus on simplification)
+                num_models = select_random(par.Incremental_drift_number, option='random')
+                result = incremental_drift_gs(tree_previous, drift_area_one, drift_area_two, num_traces, num_models, ran_evolve)
+                event_log, deleted_acs, added_acs, moved_acs, tree_list = result
+            else:
+                event_log = no_drift(tree=tree_previous, nu_traces=num_traces)
+                drift = None
 
         # ADD TIME PERSPECTIVE TO EVENT LOG
         add_duration_to_log(event_log,
@@ -78,26 +86,33 @@ def generate_logs(file_path_to_own_models=None):
                             select_random(par.Trace_exp_arrival_sec, option='uniform_int'),
                             select_random(par.Task_exp_duration_sec, option='uniform_int'))
 
-        # ADD NOISE and CREATE NOISE INFO INSTANCE
-        if noise:
-            event_log = insert_noise(event_log, par.Noisy_trace_prob[0], par.Noisy_event_prob[0])
-            noise_instance = NoiseInfo(log_name, par.Noisy_trace_prob[0], par.Noisy_event_prob[0])
-            collection.add_noise(noise_instance)
-            event_log.attributes["noise:info"] = noise_instance.noise_info_to_dict()
-
-        # CREATE DRIFT INFO INSTANCE
-        if drift:
-            drift_times = extract_change_moments_to_list(event_log)
-            drift_input = [log_name, 1, 'control-flow', drift, drift_times, added_acs, deleted_acs, moved_acs, tree_list]
-            drift_instance = initialize_drift_instance_from_list(drift_input)
-            event_log.attributes["drift:info"] = drift_instance.drift_info_to_dict()
-            collection.add_drift(drift_instance)
+        # # ADD NOISE and CREATE NOISE INFO INSTANCE
+        # noise = select_random(par.Noise, option='random')
+        # if noise:
+        #     event_log = insert_noise(event_log, par.Noisy_trace_prob[0], par.Noisy_event_prob[0])
+        #     noise_instance = NoiseInfo(log_name, par.Noisy_trace_prob[0], par.Noisy_event_prob[0])
+        #     collection.add_noise(noise_instance)
+        #     event_log.attributes[InfoTypes.noise_info.value] = noise_instance.noise_info_to_dict()
+        #
+        # # CREATE DRIFT INFO INSTANCE
+        # if drift:
+        #     drift_times = extract_change_moments_to_list(event_log)
+        #     drift_input = [log_name, 1, 'control-flow', drift, drift_times, added_acs, deleted_acs, moved_acs, tree_list]
+        #     drift_instance = initialize_drift_instance_from_list(drift_input)
+        #     event_log.attributes[InfoTypes.drift_info.value] = drift_instance.drift_info_to_dict()
+        #     collection.add_drift(drift_instance)
 
         # EXPORT GENERATED LOG
         xes_exporter.apply(event_log, os.path.join(out_folder, log_name))
 
     collection._temp_save_drift_info_to_csv_file(path=out_folder)
     print('Finished generating collection of', number_of_logs, 'logs in', out_folder)
+
+
+class InfoTypes(Enum):
+    drift_info = "drift:info"
+    noise_info = "noise:info"
+
 
 
 class DriftTypes(Enum):
