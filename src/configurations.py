660c22cacd21b19f75023a79366daf1e2aad6065
Alexander <alexander.kraus@informatik.uni-mannheim.de>
2023-03-30 13:55:17 +0200

    Updated configurations

--- src/configurations.py
+++ src/configurations.py
@@ -11,6 +11,10 @@ DEFAULT_LOG_COLLECTION_OUTPUT_DIR = os.path.join('data/generated_collections')
 ###################################################################
 # FILE NAMES
 ###################################################################
-
-
 PAR_LOG_COLLECTION = 'parameters_log_collection'
+
+
+###################################################################
+# Change type threshold value for sudden and gradual changes
+###################################################################
+CHANGE_TYPE_THRESHOLD = 0.5
