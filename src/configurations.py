a5c0b23d60a959518318474764952bfac715470b
Alexander <alexander.kraus@informatik.uni-mannheim.de>
2023-06-01 17:04:20 +0200

    STABLE VERSION (for JK)

--- src/configurations.py
+++ src/configurations.py
@@ -5,11 +5,12 @@ import os
 ###################################################################
 DEFAULT_OUTPUT_DIR = os.path.join('output')
 DEFAULT_PARAMETER_DIR = os.path.join('src/input_parameters/')
-#PARAMETER_NAME = 'default'
+PARAMETER_NAME = 'default'
+#PARAMETER_NAME = 'default_JK'
 #PARAMETER_NAME = 'experiments_simple'
 #PARAMETER_NAME = 'experiments_all_types'
 #PARAMETER_NAME = 'experiments_all_types_v2'
-PARAMETER_NAME = 'experiments_all_types_v3'
+#PARAMETER_NAME = 'experiments_all_types_v3'
 
 ###################################################################
 # FILE NAMES
