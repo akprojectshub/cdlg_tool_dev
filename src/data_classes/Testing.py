5b3155b8e0eecb194601afba007d0709b391d046
ZiedKebir <zied.kebir@students.uni-mannheim.de>
2023-07-03 11:22:53 +0200

    Implemented testing with noisy variation in the trace indexes

--- src/data_classes/Testing.py
+++ src/data_classes/Testing.py
@@ -17,64 +17,96 @@ from src.data_classes.evaluation_LP import getTP_FP
 import src.data_classes.helpers_LP
 import copy
 from src.data_classes.Automated_evaluation import Automated_evaluation
+import random
 
 from class_collection import *
 
+
+path_detected_collection = "C:/Users/ziedk/OneDrive/Bureau/Process Mining Git/output/detected"
+path_actual_collection = "C:/Users/ziedk/OneDrive/Bureau/Process Mining Git/output/actual"
+
+path_actual_csv_file = "C:/Users/ziedk/OneDrive/Bureau/Process Mining Git/output/actual/drift_info.csv"
+path_detected_csv_file = "C:/Users/ziedk/OneDrive/Bureau/Process Mining Git/output/detected/drift_info.csv"
                                             ##########################################
                                             ######## Testing (perfect case) ##########
                                             ##########################################
 
-# Actual: collection of logs; Detected: Collection of logs
-
-"""if __name__ == '__main__':
-    #Testing: Automated Evaluation
-    Col_act = Collection()
-    Col_det = Collection()
-    Col_act.Extract_collection_of_drifts("C:/Users/ziedk/OneDrive/Bureau/Process Mining Git/output/actual")
-    Col_det.Extract_collection_of_drifts("C:/Users/ziedk/OneDrive/Bureau/Process Mining Git/output/detected")
-    Automated_evaluation(Col_act, Col_det, "index_based",100)"""
+"""# Actual: collection of logs; Detected: Collection of logs
+# Testing: Automated Evaluation
+Col_act = Collection()
+Col_det = Collection()
+Col_act.Extract_collection_of_drifts(path_actual_collection)
+Col_det.Extract_collection_of_drifts(path_detected_collection)
+Automated_evaluation(Col_act, Col_det, "index_based", 100)
 
 # Actual: csv file; Detected: Collection of logs
-if __name__ == '__main__':
-    Col_act = Collection()
-    Col_act.import_drift_and_noise_info_from_flat_file_csv("C:/Users/ziedk/OneDrive/Bureau/Process Mining Git/output/actual/drift_info.csv")
-    #print("csv",Col_act.drifts[1])
+Col_act = Collection()
+Col_act.import_drift_and_noise_info_from_flat_file_csv(path_actual_csv_file)
+# print("csv",Col_act.drifts[1])
 
-    #print("################################")
-    Col_det = Collection()
-    Col_det.Extract_collection_of_drifts("C:/Users/ziedk/OneDrive/Bureau/Process Mining Git/output/detected")
+Col_det = Collection()
+Col_det.Extract_collection_of_drifts(path_detected_collection)
 
-    #print(len(Col_act.drifts))
-    #print(len(Col_det.drifts))
-    #print("fold",Col_det.drifts[1])
-    Automated_evaluation(Col_act, Col_det, "index_based",100)
+# print(len(Col_act.drifts))
+# print(len(Col_det.drifts))
+# print("fold",Col_det.drifts[1])
+Automated_evaluation(Col_act, Col_det, "index_based", 100)
 
 # Actual: collection of logs; Detected: csv file
-"""if __name__ == '__main__':
-    Col_act = Collection()
-    Col_act.import_drift_and_noise_info_from_flat_file_csv("C:/Users/ziedk/OneDrive/Bureau/Process Mining Git/output/actual")
-    Col_det = Collection()
-    Col_det.Extract_collection_of_drifts("C:/Users/ziedk/OneDrive/Bureau/Process Mining Git/output/detected/drift_info.csv")
-    print(Automated_evaluation(Col_act, Col_det, "index_based",100))
-
-"""
-
+Col_act = Collection()
+Col_act.import_drift_and_noise_info_from_flat_file_csv(path_actual_collection)
+Col_det = Collection()
+Col_det.Extract_collection_of_drifts(path_detected_csv_file)
+print(Automated_evaluation(Col_act, Col_det, "index_based", 100))
 
 # Actual: csv file; Detected: csv file
 
-"""if __name__ == '__main__':
-    Col_act = Collection()
-    Col_act.import_drift_and_noise_info_from_flat_file_csv("C:/Users/ziedk/OneDrive/Bureau/Process Mining Git/output/actual/drift_info.csv")
-    Col_det = Collection()
-    Col_det.Extract_collection_of_drifts("C:/Users/ziedk/OneDrive/Bureau/Process Mining Git/output/detected/drift_info.csv")
-    print(Automated_evaluation(Col_act, Col_det, "index_based",100))
+Col_act = Collection()
+Col_act.import_drift_and_noise_info_from_flat_file_csv(path_actual_csv_file)
+Col_det = Collection()
+Col_det.Extract_collection_of_drifts(path_detected_csv_file)
+print(Automated_evaluation(Col_act, Col_det, "index_based", 100))
+
 
 """
-
-
                                             ##########################################
-                                            ######## Testing (noisy case) ##########
+                                            ######## Testing (noisy case) ############
                                             ##########################################
 
 
+def insert_trace_index_noise(trace_index,probability_of_changing_trace, percentage_of_change):
+    if random.random() < probability_of_changing_trace:
+        if random.random() >= 0.5:
+            trace_index = trace_index * (1+percentage_of_change)
+        elif random.random() < 0.5:
+            trace_index = trace_index * (1-percentage_of_change)
+    return trace_index
+
+import copy
+
+probability_of_changing_trace = 1
+percentage_of_change = 0.7
+
+Col_act = Collection()
+Col_act.Extract_collection_of_drifts("C:/Users/ziedk/OneDrive/Bureau/Process Mining Git/output/actual")
+Col_act_modified = copy.copy(Col_act)
+
+#print("Before Change",Col_act.drifts[0][0].change_info["1"]["change_trace_index"])
+
+
+for drifts_pos in range(0,len(Col_act.drifts)):
+    for drift_pos in range(0,len(Col_act.drifts[drifts_pos])):
+        for change_info_key in list(Col_act.drifts[drifts_pos][drift_pos].change_info.keys()):
+            for change_trace_index_pos in range(0,len(Col_act.drifts[drifts_pos][drift_pos].change_info[change_info_key]["change_trace_index"])):
+                Col_act_modified.drifts[drifts_pos][drift_pos].change_info[change_info_key]["change_trace_index"][change_trace_index_pos] = insert_trace_index_noise(Col_act.drifts[drifts_pos][drift_pos].change_info[change_info_key]["change_trace_index"][change_trace_index_pos],probability_of_changing_trace, percentage_of_change)
+
+#print("After_change", Col_act_modified.drifts[0][0].change_info["1"]["change_trace_index"])
+
+
+
+# Testing: Automated Evaluation
+Col_act = Collection()
+Col_det = Collection()
+Col_det.Extract_collection_of_drifts(path_detected_collection)
+Automated_evaluation(Col_act_modified, Col_det, 100)
 
