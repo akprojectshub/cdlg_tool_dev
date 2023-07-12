6e833261ab5948d86f72dba02dc083bafa595f30
ZiedKebir <zied.kebir@students.uni-mannheim.de>
2023-07-12 18:47:10 +0200

    Commented the file class_drift

--- src/data_classes/class_drift.py
+++ src/data_classes/class_drift.py
@@ -3,12 +3,16 @@ import numpy as np
 from collections import defaultdict
 from copy import deepcopy
 import re
-
+import datetime
 import pm4py
+from pm4py.objects.log.obj import EventLog
 
 
 @dataclass
 class DriftInfo:
+    """
+        Object for keeping information about added drift to a generated event log
+    """
     log_id: str = None
     drift_id: int = np.NAN
     process_perspective: str = None
@@ -16,29 +20,47 @@ class DriftInfo:
     process_trees: dict = field(default_factory=lambda: defaultdict(dict))
     change_info: dict = field(default_factory=lambda: defaultdict(dict))
 
-    def set_log_id(self, log_name):
+    def set_log_id(self, log_name:str):
+        """
+            Add a log id to a drift instance
+        """
         self.log_id = log_name
         return None
 
-    def set_drift_id(self, drift_id):
+    def set_drift_id(self, drift_id:int):
+        """
+            Add a drift id to a drift instance
+        """
         self.drift_id = drift_id
         return None
 
 
-    def set_process_perspective(self, process_perspective):
+    def set_process_perspective(self, process_perspective:str):
+        """
+            Add a process perspective to a drift instance
+        """
         self.process_perspective = process_perspective
         return None
 
-    def set_drift_type(self, drift_type):
+    def set_drift_type(self, drift_type:str):
+        """
+            Add a drift type to a drift instance
+        """
         self.drift_type = drift_type
         return None
 
-    def add_process_tree(self, process_tree):
+    def add_process_tree(self, process_tree:dict):
+        """
+            Add a process tree to a drift instance
+        """
         process_tree_id = str(len(self.process_trees))
         self.process_trees[process_tree_id] = deepcopy(process_tree)
         return None
 
-    def add_change_info_from_csv(self,change_trace_index, change_type, tree_previous, tree_new, deleted_acs, added_acs, moved_acs, change_start, change_end):
+    def add_change_info_from_csv(self,change_trace_index:list, change_type:str, tree_previous:dict, tree_new:dict, deleted_acs:list, added_acs:list, moved_acs:list, change_start:datetime.datetime, change_end:datetime.datetime):
+        """
+            Add an information about change moment to a drift instance when the drift class is instatiated from a csv file
+        """
         self.add_process_tree(tree_new)
         change_id = str(len(self.change_info) + 1)
         self.change_info[change_id] = {'change_type': change_type,
@@ -53,8 +75,10 @@ class DriftInfo:
         return None
 
 
-    def add_change_info(self, change_trace_index, change_type, tree_previous, tree_new, deleted_acs, added_acs, moved_acs):
-
+    def add_change_info(self, change_trace_index:list, change_type:str, tree_previous:dict, tree_new:dict, deleted_acs:list, added_acs:list, moved_acs:list):
+        """
+            Add an information about change moment to a drift instance when the drift class
+        """
         self.add_process_tree(tree_new)
         change_id = str(len(self.change_info)+1)
         self.change_info[change_id] = {'change_type': change_type,
@@ -66,7 +90,7 @@ class DriftInfo:
                                        'activities_moved': moved_acs}
         return None
 
-    def convert_change_trace_index_into_timestamp(self, event_log):
+    def convert_change_trace_index_into_timestamp(self, event_log:EventLog):
         change_info_new = deepcopy(self.change_info)
         for change_id, change_data in self.change_info.items():
             for change_attr, attr_value in change_data.items():
