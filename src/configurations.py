4c56743c51eb84f10e323e2ccb1748921aaf0b13
Alexander <alexander.kraus@informatik.uni-mannheim.de>
2024-01-26 14:02:21 +0100

    Create and enable a new input_parameter for odcd

--- src/configurations.py
+++ src/configurations.py
@@ -3,8 +3,8 @@ import os
 # Default directories
 DEFAULT_OUTPUT_DIR = os.path.join('output')
 DEFAULT_PARAMETER_DIR = os.path.join('src/input_parameters/')
-PARAMETER_NAME = 'default'
+PARAMETER_NAME = 'supervised_cdd' ## 'default'
 # First time stamp of each event log
-FIRST_TIMESTAMP = '2020/01/01 08:00:00'
+FIRST_TIMESTAMP = '2023/01/01 08:00:00'
 # Incremental evolution parameters for incremental drifts
 INCREMENTAL_EVOLUTION_SCOPE = [0.05, 0.10]
